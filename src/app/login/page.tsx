547d3f6ee55750423e97889f44d903e6d53c7dff
Fer457 <fernandonevothernandez@gmail.com>
2023-11-21 21:24:37 +0100

    feat: login form styling + icons

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,11 +2,8 @@ import LoginForm from "@/app/ui/auth/LoginForm";
 
 const LoginPage = () => {
   return (
-    <main className="flex items-center justify-center md:h-screen text-black">
-      <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
-        <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
-          <div className="w-32 text-white md:w-36"></div>
-        </div>
+    <main className="flex items-center justify-center md:h-screen text-white">
+      <div className="relative mx-auto flex w-full max-w-[500px] flex-col space-y-2.5 p-4 md:-mt-32">
         <LoginForm />
       </div>
     </main>
